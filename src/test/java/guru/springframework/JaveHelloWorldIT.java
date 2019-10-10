a7963a5dc2c4874e020bf0349923b04af26670d9
Aidan Salt <ajsalt90@gmail.com>
2019-10-10 14:42:51 +0200

    Running Integration test with Maven Failsafe

---
+++ src/test/java/guru/springframework/JaveHelloWorldIT.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+class JaveHelloWorldIT {
+    @Test
+    void myFauxIntegrationTest() {
+        System.out.println("My IT Ran...");
+    }
+}
