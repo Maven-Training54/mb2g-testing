1407d73dae0a37a1f1d84db87be2ff6c69c1a707
Aidan Salt <ajsalt90@gmail.com>
2019-10-10 11:48:32 +0200

    Testing with Maven, JUnit4 and JUnit5

---
+++ src/test/java/guru/springframework/JUnit5FauxTest.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit5FauxTest {
+
+    @Test
+    void someTestForJUnit5(){
+        System.out.println("I Ran....");
+    }
+}
